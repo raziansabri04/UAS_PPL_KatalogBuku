e418f789075cd382ab06005f9c09174791aab56a
Razian Sabri <raziansabri224@gmail.com>
2026-05-31 20:08:52 +0700

    menambah filter dan buku sejarah

--- katalog/views.py
+++ katalog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Buku # Pastikan ini 'Buku'
+from .models import Buku, Kategori
 
+# Fungsi untuk Halaman Utama (dengan Filter Kategori)
 def index(request):
-    # Mengambil semua data buku
     semua_buku = Buku.objects.all().order_by('-created_at')
-    return render(request, 'katalog/index.html', {'daftar_buku': semua_buku})
+    semua_kategori = Kategori.objects.all()
+    context = {
+        'daftar_buku': semua_buku,
+        'daftar_kategori': semua_kategori,
+    }
+    return render(request, 'katalog/index.html', context)
 
+# Fungsi untuk Halaman Detail Buku (INI YANG TADI HILANG/ERROR)
 def detail_produk(request, id):
-    # Mengambil satu buku berdasarkan ID
+    # Mengambil buku berdasarkan ID, jika tidak ada kirim error 404
     buku = get_object_or_404(Buku, id=id)
     return render(request, 'katalog/detail.html', {'p': buku})
