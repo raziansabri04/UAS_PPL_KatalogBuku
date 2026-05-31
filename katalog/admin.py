149f31607be7461be54486d388fd7394173eaa80
Razian Sabri <raziansabri224@gmail.com>
2026-05-31 21:45:38 +0700

    menambah halaman beli dan mempercantik halama admin

--- katalog/admin.py
+++ katalog/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
-from .models import Kategori, Buku # Pastikan ini 'Buku', bukan 'Produk'
+from django.utils.html import format_html
+from .models import Kategori, Buku
 
 @admin.register(Buku)
 class BukuAdmin(admin.ModelAdmin):
-    # Sesuaikan list_display dengan nama field baru di model Buku
-    list_display = ('judul', 'penulis', 'harga', 'stok', 'kategori') 
-    search_fields = ('judul', 'penulis')
+    # Menambahkan 'show_sampul' di daftar kolom
+    list_display = ('show_sampul', 'judul', 'penulis', 'kategori', 'harga', 'stok')
     list_filter = ('kategori',)
+    search_fields = ('judul', 'penulis')
+
+    # Fungsi untuk menampilkan foto kecil
+    def show_sampul(self, obj):
+        if obj.sampul:
+            return format_html('<img src="{}" style="width: 50px; height: auto; border-radius: 5px;" />', obj.sampul.url)
+        return "No Image"
+    
+    show_sampul.short_description = 'Sampul'
 
 admin.site.register(Kategori)
-
-# Opsional: Kustomisasi judul dashboard
-admin.site.site_header = "Panel Admin Katalog Buku"
-admin.site.index_title = "Manajemen Koleksi Buku"
